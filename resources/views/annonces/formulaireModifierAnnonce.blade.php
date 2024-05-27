a82ffeeee65b5169e3187537c1d4085f5e49fa3f
abdelhak5555 <moussaaouiabdelhak@gmail.com>
2024-05-27 22:08:12 +0100

    corrig err de modfiie annoce

--- resources/views/annonces/formulaireModifierAnnonce.blade.php
+++ resources/views/annonces/formulaireModifierAnnonce.blade.php
@@ -2,183 +2,17 @@
 <html lang="fr">
 
 <head>
-    <script src="{{ asset('assets\js\App.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('assets\css\styles.css') }}" />
-    <link rel="icon" href="{{ asset('assets\images\logoT.svg') }}">
+    <script src="{{ asset('assets/js/App.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}" />
+    <link rel="icon" href="{{ asset('assets/images/logoT.svg') }}">
     <title>Tabaraa</title>
-
 </head>
 
-
 <body id="page-signup">
+
     <header class="tabaraa-header">
-        <div class="head-top"></div>
-        <div class="toolsbar-dummy"></div>
-        <div class="toolsbar f-container f-align-stretch">
-            <div class="limiter-large">
-                <div class="f-container f-wrap-nowrap f-item" id="main-appbar">
-                    <div class="mq-sm-hidden">
-                        <button title="Menu" class="repliable ham-toggle" data-target="nav-global">
-                            <span class="shell global-shell">
-                                <i class="fa fa-bars fa-lg"></i>
-                            </span>
-                        </button>
-                    </div>
-                    <div class="f-container">
-                        <div class="f-align-self-start">
-                            <a class="f-container logo-picto" href="{{ route('userHome') }}" title="Site de don d'objets">
-                                <img class="logo f-align-self-center" height="20" width="20" src="{{ asset('assets\images\logo1icondonation.png') }}" loading="lazy" decoding="async" alt="Tabaraa" />
-                            </a>
-                            <a class="f-container logo-full" href="{{ route('userHome') }}" title="Site de don d'objets">
-                                <img class="logo f-align-self-center" height="50" width="100" src="{{ asset('assets\images\Tabaraalogo.svg') }}" loading="lazy" decoding="async" alt="Tabaraa" />
-                            </a>
-                        </div>
-                    </div>
-                    <div class="f-grow-1 f-container f-wrap-nowrap f-align-center f-content-center ">
-                        <div class="header-search f-content-between" id="header-search-loc">
-                            <span class="header-search-resume">
-                                <span class="header-search-resume-emphasis">Rechercher un don...</span>
-                                <span class="header-search-resume-secondary"></span>
-                            </span>
-                            <button type="button" aria-label="Recherche"><i class="fa fa-search"></i></button>
-                        </div>
-                    </div>
-
-                    <div class="f-grow-1 f-container f-wrap-nowrap f-align-center f-content-end">
-                        <button title="Ma messagerie" class="repliable ham-toggle btn-light position-relative" onclick="markAllMessagesAsRead()">
-                            <a href="{{ route('messages') }}" class="text-decoration-none">
-                                <span class="shell msgs-shell position-relative d-inline-block">
-                                    <i class="fa fa-envelope fa-lg position-relative d-block mx-auto"></i>
-                                    <span id="message-counter" class="badge badge-pill badge-primary bg-danger position-absolute top-0 start-100 translate-middle p-1"></span>
-                                </span>
-                            </a>
-                        </button>
-
-                        <script src="{{ asset('assets\js\jQuery.js') }}"></script> <!-- Assurez-vous d'inclure jQuery -->
-
-                        <script>
-                            // Fonction pour marquer tous les messages comme lus
-                            function markAllMessagesAsRead() {
-                                $.ajax({
-                                    url: "{{ route('markAllMessagesAsRead') }}",
-                                    method: "POST",
-                                    success: function(data) {
-                                        console.log(data); // Ajoutez cette ligne pour afficher le résultat dans la console
-                                        if (data.success) {
-                                            // Mettre à jour le compteur de messages
-                                            $('#message-counter').text('').removeClass('show'); // Retire le compteur de messages
-                                        } else {
-                                            console.error('Erreur lors de la mise à jour des messages.');
-                                        }
-                                    },
-                                    error: function(xhr, status, error) {
-                                        console.error(error);
-                                    }
-                                });
-                            }
-                            // Fonction pour mettre à jour le compteur de messages
-                            function updateMessageCounter() {
-                                $.ajax({
-                                    url: "{{ route('getMessageCount') }}",
-                                    method: "GET",
-                                    success: function(data) {
-                                        if (data.count > 0) {
-                                            $('#message-counter').text(data.count).addClass('show'); // Ajoute la classe 'show' si le compteur est supérieur à 0
-                                        } else {
-                                            $('#message-counter').removeClass('show'); // Retire la classe 'show' si le compteur est égal à 0
-                                        }
-                                    },
-                                    error: function(xhr, status, error) {
-                                        console.error(error);
-                                    }
-                                });
-                            }
-
-                            // Appeler la fonction pour mettre à jour le compteur au chargement de la page
-                            $(document).ready(function() {
-                                updateMessageCounter();
-                            });
-                        </script>
-
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-                        <button title="Accès à mon compte" class="repliable ham-toggle" data-target="nav-user">
-                            <span class="shell">
-                                @php $utilisateur = auth()->user(); @endphp
-                                @if($utilisateur)
-                                <img src="{{ asset('storage/' . $utilisateur->photo_profile) }}" loading="lazy" decoding="async" alt="Mon avatar">
-                                @else
-                                <img src="{{ asset('assets\images\avatar.png') }}" loading="lazy" decoding="async" alt="Mon avatar">
-                                @endif
-                            </span>
-                        </button>
-
-                    </div>
-
-                    <!-- <div class="f-grow-1 f-container f-wrap-nowrap f-align-end f-content-end">
-                        <div class="mq-sm-visible">
-                            <div class="f-container f-align-center">
-                                
-                                <button title="Ma messagerie" class="repliable ham-toggle" data-target="nav-mails">
-                                    <a href="message.html">
-                                        <span class="shell msgs-shell">
-                                        <i class="fa fa-envelope fa-lg"></i>
-                                    </span>
-                                    </a>
-                                </button>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-                                <button title="Accès à mon compte" class="repliable ham-toggle" data-target="nav-user">
-                                    <span class="shell">
-                                        <img src="avatar.png" loading="lazy" decoding="async" alt="Mon avatar">
-                                    </span>
-                                </button>
-                            </div>
-                        </div>                   
-                    </div>  -->
-
-                </div>
-                <div class="f-container f-content-center spaced" id="global-search">
-                    <div class="f-item">
-                        <div id="search-header-appbar">
-                            <button id="close-search-header"><i class="fa fa-chevron-left"></i></button>
-                            <span>Ma recherche</span>
-                        </div>
-                        <form action="{{ route('rechercher') }}" method="POST">
-                            @csrf
-                            <div class="f-container pt-sm search-filter-zone text-sm">
-                                <div class="search-cell">
-                                    <label for="search-header-keywords" class="search-input-label">Que recherchez-vous ?</label>
-                                    <input class="select2" name="termes" id="search-header-keywords" type="text" maxlength="20" placeholder="Table,Pull..." value="" />
-                                </div>
-                                <div class="search-cell">
-                                    <label for="search-header-keywords" class="search-input-label">Sur quelle ville ?</label>
-                                    <input class="select2" name="ville" id="search-header-keywords" type="text" maxlength="50" placeholder="Tlemcen,Oran.." value="" />
-                                    <!-- <span class="search-header-reset-keywords disabled"><i class="fa fa-close"></i></span> -->
-                                </div>
-                                <div class="search-cell">
-                                    <button type="submit" id="search-header-submit" class="search-valid search" data-target="#search-header-alert">
-                                        <i class="fa fa-search"></i>
-                                        Rechercher
-                                        <!-- <span class="search-header-nb-results"></span> -->
-                                        <div id="search-header-alert" class="text-xs text-center display-soft-none">
-                                            Veuillez sélectionner au moins un critère ci-dessus
-                                        </div>
-                                    </button>
-                                    <div class="pt-sm text-center bloc_saved_search has-recherches display-none">
-                                        <button class="btn outline  open-saved-search">Mes recherches</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <button id="global-search-close" class="icon-btn "><i class="fa fa-arrow-up"></i></button>
-                </div>
-            </div>
-        </div>
+        @include('components.userAuth.header')
     </header>
-
-
 
     <div class="ham-nav ham-right" id="nav-user" data-icon="user" data-title="Tabaraa">
         <div class="title">
@@ -193,6 +27,7 @@
             <a href="{{ route('afficherFormulaireConnexion') }}" rel="nofollow">Se déconnecter</a>
         </div>
     </div>
+    
     <div class="ham-nav ham-right" id="nav-global" data-icon="bell" data-title="Tabaraa">
         <div class="title">
             <i class="fa fa-bars"></i>
@@ -201,10 +36,8 @@
         </div>
         <div class="ham-content" id="notifs-content">
             <br>
-            <a href="{{ route('afficherFormulaireConnexion') }}" title="Me connecter à mon compte"><i class="fa fa-user fa-lg"></i>&nbsp; &nbsp; Se connecter
-            </a>
-            <a href="{{ route('afficherFormulaireInscription') }}" title="Créer un compte"><i class="fa-solid fa-user-plus fa-lg"></i>&nbsp; &nbsp; S'inscrire
-            </a>
+            <a href="{{ route('afficherFormulaireConnexion') }}" title="Me connecter à mon compte"><i class="fa fa-user fa-lg"></i>&nbsp; &nbsp; Se connecter</a>
+            <a href="{{ route('afficherFormulaireInscription') }}" title="Créer un compte"><i class="fa-solid fa-user-plus fa-lg"></i>&nbsp; &nbsp; S'inscrire</a>
             <br>
             <span>Catégories</span>
             <a href="{{ route('vetement') }}" title="vetements">&nbsp; &nbsp;<i class="fa-sharp fa-solid fa-shirt fa-xl" style="color: #08680f;"></i>&nbsp; &nbsp;Vetements</a>
@@ -213,10 +46,8 @@
             <a href="{{ route('article_maison') }}" title="artice_maison">&nbsp; &nbsp; <i class="fa-solid fa-house-chimney fa-lg" style="color: #3c3b3f;"></i> &nbsp; &nbsp;Articles Maison</a>
             <a href="{{ route('auto') }}" title="pieces_auto">&nbsp; &nbsp; <i class="fa-solid fa-car fa-xl" style="color: #354c73;"></i> &nbsp; &nbsp; Pièces Automobiles</a>
             <a href="{{ route('autre') }}" title="autres">&nbsp; &nbsp; <i class="fa-brands fa-slack fa-xl" style="color: #3a2612;"></i>&nbsp; &nbsp;&nbsp; Autres</a>
-
         </div>
     </div>
-
 
     <div class="global-shadow"></div>
     <div class="deco-bg">
@@ -308,23 +139,65 @@
             </form>
         </div>
 
-        <script>
-            function showSuccessMessage() {
-                var data = {
-                    code: 1
-                };
-    
-                // Vérifiez si la réponse de la requête est 1 (succès)
-                if (data.code === 1) {
-                    // Affichage d'un message de succès avec SweetAlert
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script>
+    // Définition de la fonction showSuccessMessage
+    function showSuccessMessage() {
+        // Code pour afficher un message de succès
+        // Par exemple, vous pouvez utiliser Swal.fire pour afficher une alerte
+        Swal.fire({
+            icon: 'success',
+            title: 'Succès!',
+            text: 'Annonce modifiée avec succès!',
+            timer: 3000, // temps en millisecondes
+            timerProgressBar: true,
+            willClose: () => {
+                // Rediriger vers la page spécifique après la modification réussie de l'annonce
+                window.location.href = "{{ route('mesAnnonces') }}";
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        $('#formAnnonce').on('submit', function(event) {
+            event.preventDefault();
+
+            var formData = new FormData(this);
+
+            $.ajax({
+                url: $(this).attr('action'),
+                method: $(this).attr('method'),
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    if (response.code === 1) {
+                        $('#content-form').hide();
+                        // Appeler la fonction showSuccessMessage lorsque la modification est réussie
+                        showSuccessMessage();
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: response.message // Afficher le message d'erreur retourné par le serveur
+                        });
+                    }
+                },
+                error: function() {
                     Swal.fire({
-                        title: "Annonce créée avec succès !",
-                        icon: "success",
-                        timer: 5000
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Une erreur est survenue. Veuillez réessayer.'
                     });
                 }
-            }
-        </script>
+            });
+        });
+    });
+</script>
+
+
 
         <div id="dbalpha"></div>
         <div id="dialogBoxContent"></div>
@@ -332,7 +205,7 @@
         <div class="page-bottom">
             <div class="container">
                 <div class="page-bottom-promote">
-                    <img class="logo f-align-self-center" width="200" height="85" src="{{ asset('assets\images\logo1icondonation.png') }}" loading="lazy" decoding="async" alt="" /><br>
+                    <img class="logo f-align-self-center" width="200" height="85" src="{{ asset('assets/images/logo1icondonation.png') }}" loading="lazy" decoding="async" alt="" /><br>
                     <span class="pt-none mt-none ml-none"><span class="text-orange">Nous donnons des dons à toutes </span>
                         &nbsp;
                         <span class="text-blue-light">personnes étant dans le besoin</span></span>
@@ -354,10 +227,10 @@
             "API_Modules": "{}"
         };
     </script>
-    <script src="{{ asset('assets\js\script1.js') }}"></script>
-    <script src="{{ asset('assets\js\script2.js') }}"></script>
-    <script src="{{ asset('assets\js\script3.js') }}"></script>
-    <script src="{{ asset('assets\js\user.js') }}"></script>
+    <script src="{{ asset('assets/js/script1.js') }}"></script>
+    <script src="{{ asset('assets/js/script2.js') }}"></script>
+    <script src="{{ asset('assets/js/script3.js') }}"></script>
+    <script src="{{ asset('assets/js/user.js') }}"></script>
 </body>
 
 </html>
